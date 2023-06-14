2ab9d59db15f76df9252af88fbb5573c035e63ca
zaraazzar <azzar.arza18@gmail.com>
2023-06-14 23:18:12 +0700

    publish

---
+++ home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=#, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div id="app">
+        <nav>
+            <ul> 
+                <li><a href="register.php"> Registrasi</a></li>
+                <li><a href="login.php">Masuk</a></li>
+                <li><a href="map.php">Map >></a></li>
+            </ul>
+        </nav>
+        <h1> WELCOME! </h1>
+        <div class="head">
+            <p>Sistem Informasi Zona Nilai Tanah Desa Bener<p>
---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="Chrome">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <div id=apps>
+        <h1>Welcome to Sistem Informasi Zona Nilai Tanah Desa Bener</h1>
+        
+        <form action="" method="post">
+            <input type="text" name="username" class="input" placeholder="Masukan Username..." /><br /><br />
+            <input type="password" name="password" class="input" placeholder="Masukan Password" /><br /><br />
+            <input type="submit" name="login" value="Masuk ke Sistem" class="form-btn">
+            <p>hanya pengunjung biasa? <a href="home.php">Kembali ke Halaman Home</a></p>       
+        <form>
+    </div>
+</body>
+
+</html>
